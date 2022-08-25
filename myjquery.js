9db394ef4810e44629e0a92bc139ba8ed5a23827
godswill udemba <godswill.udemna.pg88009@unn.edu.ng>
2022-08-25 12:06:34 +0100

    second commit

--- myjquery.js
+++ myjquery.js
@@ -22,3 +22,20 @@ $("ul li:odd").css("background-color", "gray");
 $("[href]").css("color", "#65702");
 //if we wanrt to target a particular link
 $('a[href="https://yahoo.com"]').css("color", "red");
+
+//Click event
+/*note if you want your want to put your script on the same page
+with your doccument, you make sure you inbade $(document).ready(function(){
+  block of codes.
+})
+this will make the js to run only when the web has loaded fully.*/
+
+/*$(".btn1").click(function () {
+  alert("button clicked");
+}); //their is an error*/
+
+//$(".btn1").on("click", function () {
+// $(".Para1.1").hide();
+//});
+
+//DOM  MANIPULATION
